af39094e9d62f01ef335e1daeb417a1f75d99039
thedannysoto <87dsoto@gmail.com>
2021-11-17 00:26:08 -0600

    Create nextEmptySquare function and start to build out main decode function

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,8 +1,8 @@
 // variable to fill empty spaces
 const e = null;
 
-// empty board
-const emptyBoard = [
+// empty grid
+const emptyGrid = [
     [e, e, e, e, e, e, e, e, e],
     [e, e, e, e, e, e, e, e, e],
     [e, e, e, e, e, e, e, e, e],
@@ -13,7 +13,6 @@ const emptyBoard = [
     [e, e, e, e, e, e, e, e, e],
     [e, e, e, e, e, e, e, e, e]
 ];
-
 
 function isSolved(grid) {
     // loop through each square and check if all numbers are filled in
@@ -26,4 +25,31 @@ function isSolved(grid) {
         }
     }
     return true;
+}
+
+
+function decode (grid) {
+    // first check for solved grid
+    if (isSolved(grid)) {
+        return grid;
+    } else {
+        // Find empty square and test numbers 1-9
+        const options = nextOptions(grid);
+    }
+}
+
+function nextOptions (grid) {
+    let testGrids = [];
+
+}
+
+function nextEmptySquare (grid) {
+    // Loop through grid and return coordinates of next empty square
+    for (let x = 0; x < 9; x++) {
+        for (let y = 0; y < 9; y++) {
+            if (grid[x][y] == null) {
+                return [x, y];
+            }
+        }
+    }
 }
